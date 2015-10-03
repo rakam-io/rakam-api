809b202c7ffc804be1e327a1ef395117abae71d0
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-03 18:47:11 +0300

    fix unused dependency error

--- rakam-presto/src/main/java/org/rakam/report/PrestoModule.java
+++ rakam-presto/src/main/java/org/rakam/report/PrestoModule.java
@@ -15,6 +15,7 @@ import org.rakam.plugin.*;
 
 import javax.inject.Inject;
 
+import static com.google.common.base.Preconditions.checkState;
 import static io.airlift.configuration.ConfigurationModule.bindConfig;
 
 @AutoService(RakamModule.class)
@@ -30,6 +31,8 @@ public class PrestoModule extends RakamModule {
         binder.bind(MaterializedViewService.class).to(PrestoMaterializedViewService.class);
 
         JDBCConfig config = buildConfigObject(JDBCConfig.class, "presto.metastore.jdbc");
+
+        checkState(org.postgresql.Driver.isRegistered());
 
         JDBCPoolDataSource dataSource = new JDBCPoolDataSource(config);
         binder.bind(JDBCPoolDataSource.class)
