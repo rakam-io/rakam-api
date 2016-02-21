3e08533c8b8e81c2c041cd106dc700b63e06a276
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-02-21 03:11:24 +0200

    do not fail even if there's not implementation of UserActionService

--- rakam/src/main/java/org/rakam/automation/AutomationModule.java
+++ rakam/src/main/java/org/rakam/automation/AutomationModule.java
@@ -5,6 +5,7 @@ import com.google.auto.service.AutoService;
 import com.google.inject.Binder;
 import com.google.inject.multibindings.Multibinder;
 import org.rakam.config.EncryptionConfig;
+import org.rakam.plugin.user.UserActionService;
 import org.rakam.util.ConditionalModule;
 import org.rakam.plugin.EventProcessor;
 import org.rakam.plugin.RakamModule;
@@ -20,6 +21,7 @@ public class AutomationModule extends RakamModule {
         configBinder(binder).bindConfig(EncryptionConfig.class);
         Multibinder<EventProcessor> eventProcessors = Multibinder.newSetBinder(binder, EventProcessor.class);
         eventProcessors.addBinding().to(AutomationEventProcessor.class);
+        Multibinder.newSetBinder(binder, UserActionService.class);
 
         Multibinder<AutomationAction> automationActions = Multibinder.newSetBinder(binder, AutomationAction.class);
         for (AutomationActionType automationActionType : AutomationActionType.values()) {
