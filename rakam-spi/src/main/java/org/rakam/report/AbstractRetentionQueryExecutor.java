db2171a94b1b838b50a9b4c921c6b4247f41f144
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-03-22 20:18:18 +0200

    fix retention query

--- rakam-spi/src/main/java/org/rakam/report/AbstractRetentionQueryExecutor.java
+++ rakam-spi/src/main/java/org/rakam/report/AbstractRetentionQueryExecutor.java
@@ -108,7 +108,7 @@ public abstract class AbstractRetentionQueryExecutor implements RetentionQueryEx
                         "  %s\n" +
                         ") \n" +
                         "select %s, cast(null as bigint) as lead, cardinality(%s(%s_set)) count from first_action data %s union all\n" +
-                        "SELECT * FROM (select %s, %s, cardinality_intersection(%s(data.%s_set), %s(returning_action.%s_set)) count \n" +
+                        "SELECT * FROM (select %s, %s - 1, cardinality_intersection(%s(data.%s_set), %s(returning_action.%s_set)) count \n" +
                         "from first_action data join returning_action on (data.date < returning_action.date AND data.date + interval '%d' day > returning_action.date) \n" +
                         "%s) WHERE count > 0 ORDER BY 1, 2 NULLS FIRST",
                 firstActionQuery, returningActionQuery, dimensionColumn, mergeSetAggregation,
