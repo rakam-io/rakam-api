ccadd76b5041673e82011a1b62d375704c511e5b
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-09-07 16:53:17 +0300

    do not use pool

--- rakam-spi/src/main/java/org/rakam/analysis/JDBCPoolDataSource.java
+++ rakam-spi/src/main/java/org/rakam/analysis/JDBCPoolDataSource.java
@@ -63,8 +63,8 @@ public class JDBCPoolDataSource implements DataSource {
 
     @Override
     public Connection getConnection() throws SQLException {
-//        return DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword());
-        return dataSource.getConnection();
+        return DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword());
+//        return dataSource.getConnection();
     }
 
     @Override
