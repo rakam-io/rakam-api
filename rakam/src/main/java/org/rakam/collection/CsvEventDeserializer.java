6633284a3fb537a490513a063ec8a4fdae98f808
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-09-25 01:30:07 +0300

    make useHeader in CSV parser optional

--- rakam/src/main/java/org/rakam/collection/CsvEventDeserializer.java
+++ rakam/src/main/java/org/rakam/collection/CsvEventDeserializer.java
@@ -67,7 +67,7 @@ public class CsvEventDeserializer
         String collection = (String) ctxt.getAttribute("collection");
         String apiKey = (String) ctxt.getAttribute("apiKey");
 
-        boolean useheader = (boolean) ctxt.getAttribute("useHeader");
+        boolean useheader = Boolean.TRUE == ctxt.getAttribute("useHeader");
 
         Map.Entry<List<SchemaField>, int[]> header;
         if (useheader) {
