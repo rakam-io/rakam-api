4033c5fbd4dfd020200516e73fee7a7bd919c9b8
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-09-26 15:43:53 +0300

    use default maximum lifecycle in hikari

--- rakam-spi/src/main/java/org/rakam/analysis/JDBCPoolDataSource.java
+++ rakam-spi/src/main/java/org/rakam/analysis/JDBCPoolDataSource.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.rakam.config.JDBCConfig;
 
 import javax.sql.DataSource;
+
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,12 +19,15 @@ import java.util.logging.Logger;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-public class JDBCPoolDataSource implements DataSource {
+public class JDBCPoolDataSource
+        implements DataSource
+{
     private static final Map<JDBCConfig, JDBCPoolDataSource> pools = new ConcurrentHashMap<>();
     private final HikariDataSource dataSource;
     private final JDBCConfig config;
 
-    private JDBCPoolDataSource(JDBCConfig config, Optional<String> initialString) {
+    private JDBCPoolDataSource(JDBCConfig config, Optional<String> initialString)
+    {
         this.config = config;
         checkArgument(config.getUrl() != null, "JDBC url is required");
 
@@ -34,82 +38,100 @@ public class JDBCPoolDataSource implements DataSource {
 
         if (config.getConnectionIdleTimeout() != null) {
             hikariConfig.setIdleTimeout(config.getConnectionIdleTimeout());
-        } else {
-            hikariConfig.setIdleTimeout(1000);
         }
 
         if (config.getMaxConnection() != null) {
             hikariConfig.setMaximumPoolSize(config.getMaxConnection());
-        } else {
+        }
+        else {
             hikariConfig.setMaximumPoolSize(30);
         }
 
 //        hikariConfig.setMaxLifetime(60000);
-
 //        hikariConfig.setConnectionTimeout(10000);
 //        hikariConfig.setLeakDetectionThreshold(2000);
 
         hikariConfig.setAutoCommit(true);
         hikariConfig.setPoolName("generic-jdbc-query-executor");
-        if(initialString.isPresent()) {
+        if (initialString.isPresent()) {
             hikariConfig.setConnectionInitSql(initialString.get());
         }
         dataSource = new HikariDataSource(hikariConfig);
     }
 
-    public static JDBCPoolDataSource getOrCreateDataSource(JDBCConfig config, String initialQuery) {
+    public static JDBCPoolDataSource getOrCreateDataSource(JDBCConfig config, String initialQuery)
+    {
         return pools.computeIfAbsent(config,
                 key -> new JDBCPoolDataSource(config, Optional.of(initialQuery)));
     }
 
-    public static JDBCPoolDataSource getOrCreateDataSource(JDBCConfig config) {
+    public static JDBCPoolDataSource getOrCreateDataSource(JDBCConfig config)
+    {
         return pools.computeIfAbsent(config,
                 key -> new JDBCPoolDataSource(config, Optional.empty()));
     }
 
     @Override
-    public Connection getConnection() throws SQLException {
+    public Connection getConnection()
+            throws SQLException
+    {
 //        return DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword());
         return dataSource.getConnection();
     }
 
     @Override
-    public Connection getConnection(String username, String password) throws SQLException {
+    public Connection getConnection(String username, String password)
+            throws SQLException
+    {
         return dataSource.getConnection(username, password);
     }
 
     @Override
-    public PrintWriter getLogWriter() throws SQLException {
+    public PrintWriter getLogWriter()
+            throws SQLException
+    {
         return dataSource.getLogWriter();
     }
 
     @Override
-    public void setLogWriter(PrintWriter out) throws SQLException {
+    public void setLogWriter(PrintWriter out)
+            throws SQLException
+    {
         dataSource.setLogWriter(out);
     }
 
     @Override
-    public void setLoginTimeout(int seconds) throws SQLException {
+    public void setLoginTimeout(int seconds)
+            throws SQLException
+    {
         dataSource.setLoginTimeout(seconds);
     }
 
     @Override
-    public int getLoginTimeout() throws SQLException {
+    public int getLoginTimeout()
+            throws SQLException
+    {
         return dataSource.getLoginTimeout();
     }
 
     @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+    public Logger getParentLogger()
+            throws SQLFeatureNotSupportedException
+    {
         return dataSource.getParentLogger();
     }
 
     @Override
-    public <T> T unwrap(Class<T> iface) throws SQLException {
+    public <T> T unwrap(Class<T> iface)
+            throws SQLException
+    {
         return dataSource.unwrap(iface);
     }
 
     @Override
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+    public boolean isWrapperFor(Class<?> iface)
+            throws SQLException
+    {
         return dataSource.isWrapperFor(iface);
     }
 }
