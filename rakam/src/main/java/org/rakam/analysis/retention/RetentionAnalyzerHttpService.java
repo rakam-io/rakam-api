e185fb9a9e13c893333a079afab5f7bf20ac5cec
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-11-09 11:23:23 +0300

    Revert commit

--- rakam/src/main/java/org/rakam/analysis/retention/RetentionAnalyzerHttpService.java
+++ rakam/src/main/java/org/rakam/analysis/retention/RetentionAnalyzerHttpService.java
@@ -131,7 +131,7 @@ public class RetentionAnalyzerHttpService
             this.period = period;
             this.startDate = startDate;
             this.endDate = endDate;
-            this.approximate = !Boolean.FALSE.equals(approximate);
+            this.approximate = Boolean.TRUE.equals(approximate);
             try {
                 this.timezone = Optional.ofNullable(timezone)
                         .map(t -> ZoneId.of(t))
