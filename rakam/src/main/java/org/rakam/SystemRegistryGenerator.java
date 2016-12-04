3c9a8d1566089086bcc04866978bc08398f463f7
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-12-05 02:43:34 +0300

    Do not fail if the registry.json file could not be created

--- rakam/src/main/java/org/rakam/SystemRegistryGenerator.java
+++ rakam/src/main/java/org/rakam/SystemRegistryGenerator.java
@@ -17,56 +17,61 @@ public final class SystemRegistryGenerator {
     }
 
     public static void main(String[] args) throws IOException {
-        if(args.length != 1 || !args[0].equals("json") && !args[0].equals("properties")) {
-            System.err.println("Usage: [json] or [properties]");
-            System.exit(1);
-        }
-        Set<Module> allModules = ServiceStarter.getModules();
-        SystemRegistry systemRegistry = new SystemRegistry(allModules, allModules);
-        if(args[0].equals("json")) {
-            System.out.print(JsonHelper.encode(systemRegistry));
-        } else {
-
-            PrintWriter printWriter = new PrintWriter(System.out);
-
-            for (SystemRegistry.ModuleDescriptor moduleDescriptor : systemRegistry.getModules()) {
-                String name;
-                if(moduleDescriptor.name == null) {
-                    name = moduleDescriptor.className;
-                } else {
-                    name = moduleDescriptor.name;
-                }
-
-                printWriter.println("#------------------------------------------------------------------------------");
-                printWriter.println("#"+name.toUpperCase(Locale.ENGLISH));
-                if (moduleDescriptor.description != null) {
-                    printWriter.println("#" + moduleDescriptor.description);
-                }
-                printWriter.println("#------------------------------------------------------------------------------");
-
-                if (moduleDescriptor.condition.isPresent()) {
-                    Condition condition = moduleDescriptor.condition.get();
-                    printWriter.println("#Condition for this plugin to be is_active:");
-
-                    if (condition.expectedValue.isEmpty()) {
-                        printWriter.println("#" + condition.property + " property must be set");
-                    } else {
-                        printWriter.println("#" + condition.property + "=" + condition.expectedValue + "\n");
-                    }
-                }
-
-                for (SystemRegistry.ConfigItem property : moduleDescriptor.properties) {
-                    if (property.description != null && !property.description.isEmpty()) {
-                        // TODO: support for breaking words to multiple lines
-                        printWriter.println("# " + property.description);
-                    }
-                    String value = property.defaultValue.equals("null") ? "" : property.defaultValue;
-                    printWriter.println("#" + property.property + "=" + value);
-                }
-
-                printWriter.print("\n\n");
+        try {
+            if(args.length != 1 || !args[0].equals("json") && !args[0].equals("properties")) {
+                System.err.println("Usage: [json] or [properties]");
+                System.exit(1);
             }
-            printWriter.flush();
+            Set<Module> allModules = ServiceStarter.getModules();
+            SystemRegistry systemRegistry = new SystemRegistry(allModules, allModules);
+            if(args[0].equals("json")) {
+                System.out.print(JsonHelper.encode(systemRegistry));
+            } else {
+
+                PrintWriter printWriter = new PrintWriter(System.out);
+
+                for (SystemRegistry.ModuleDescriptor moduleDescriptor : systemRegistry.getModules()) {
+                    String name;
+                    if(moduleDescriptor.name == null) {
+                        name = moduleDescriptor.className;
+                    } else {
+                        name = moduleDescriptor.name;
+                    }
+
+                    printWriter.println("#------------------------------------------------------------------------------");
+                    printWriter.println("#"+name.toUpperCase(Locale.ENGLISH));
+                    if (moduleDescriptor.description != null) {
+                        printWriter.println("#" + moduleDescriptor.description);
+                    }
+                    printWriter.println("#------------------------------------------------------------------------------");
+
+                    if (moduleDescriptor.condition.isPresent()) {
+                        Condition condition = moduleDescriptor.condition.get();
+                        printWriter.println("#Condition for this plugin to be is_active:");
+
+                        if (condition.expectedValue.isEmpty()) {
+                            printWriter.println("#" + condition.property + " property must be set");
+                        } else {
+                            printWriter.println("#" + condition.property + "=" + condition.expectedValue + "\n");
+                        }
+                    }
+
+                    for (SystemRegistry.ConfigItem property : moduleDescriptor.properties) {
+                        if (property.description != null && !property.description.isEmpty()) {
+                            // TODO: support for breaking words to multiple lines
+                            printWriter.println("# " + property.description);
+                        }
+                        String value = property.defaultValue.equals("null") ? "" : property.defaultValue;
+                        printWriter.println("#" + property.property + "=" + value);
+                    }
+
+                    printWriter.print("\n\n");
+                }
+                printWriter.flush();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
