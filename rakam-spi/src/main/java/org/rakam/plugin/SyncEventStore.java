9e41db039429b10891a7bee008e88c6c14e13bb3
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-01-24 10:55:03 +0300

    Use dedicated thread pool executor for SyncEventStore

--- rakam-spi/src/main/java/org/rakam/plugin/SyncEventStore.java
+++ rakam-spi/src/main/java/org/rakam/plugin/SyncEventStore.java
@@ -1,30 +1,34 @@
 package org.rakam.plugin;
 
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
 import org.rakam.collection.Event;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public interface SyncEventStore
         extends EventStore
 {
-    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    ThreadPoolExecutor workerGroup = new ThreadPoolExecutor(0, Runtime.getRuntime().availableProcessors() * 6,
+            60L, TimeUnit.SECONDS,
+            new SynchronousQueue<>());
 
-    default CompletableFuture<Void> storeAsync(Event event) {
+    default CompletableFuture<Void> storeAsync(Event event)
+    {
         return CompletableFuture.supplyAsync(() -> {
             store(event);
             return null;
         }, workerGroup);
     }
 
-    default CompletableFuture<int[]> storeBatchAsync(List<Event> events) {
+    default CompletableFuture<int[]> storeBatchAsync(List<Event> events)
+    {
         return CompletableFuture.supplyAsync(() -> storeBatch(events), workerGroup);
-
-
     }
 
     void store(Event event);
+
     int[] storeBatch(List<Event> events);
 }
