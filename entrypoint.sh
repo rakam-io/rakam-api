7e0b64650d587139f3ba67b1ab7b311be4dcb204
ilker cam <ilker.cam@isik.edu.tr>
2019-11-13 21:09:08 +0300

    fix custom flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 echo "$1"
 if [ $1 == "custom" ]
-   java $JAVA_OPTS -cp /app/lib/*: -Dstore.adapter=postgresql -Dplugin.user.enabled=true -Devent-explorer.enabled=true -Dcustom-data-source.enabled=true -Duser.funnel-analysis.enabled=true -Dplugin.user.enable-user-mapping=true -Duser.retention-analysis.enabled=true -Dplugin.geoip.enabled=true -Dplugin.user.storage=postgresql -Dhttp.server.address=0.0.0.0:9999 -Dplugin.user.storage.identifier-column=id -Dplugin.geoip.database.url=file://tmp/GeoLite2-City.mmdb org.rakam.ServiceStarter
+   java $JAVA_OPTS -cp /app/lib/*: -Dplugin.geoip.enabled=true -Dplugin.geoip.database.url=file://tmp/GeoLite2-City.mmdb org.rakam.ServiceStarter
 then
-   java $JAVA_OPTS -cp /app/lib/*: -Dstore.adapter=postgresql -Dplugin.user.enabled=true -Devent-explorer.enabled=true -Dcustom-data-source.enabled=true -Duser.funnel-analysis.enabled=true -Dplugin.user.enable-user=true org.rakam.ServiceStarter
+   java $JAVA_OPTS -cp /app/lib/*: -Dstore.adapter=postgresql -Dplugin.user.enabled=true -Devent-explorer.enabled=true -Dcustom-data-source.enabled=true -Duser.funnel-analysis.enabled=true -Dplugin.user.enable-user-mapping=true -Duser.retention-analysis.enabled=true -Dplugin.geoip.enabled=true -Dplugin.user.storage=postgresql -Dhttp.server.address=0.0.0.0:9999 -Dplugin.user.storage.identifier-column=id -Dplugin.geoip.database.url=file://tmp/GeoLite2-City.mmdb org.rakam.ServiceStarter
 fi
 
